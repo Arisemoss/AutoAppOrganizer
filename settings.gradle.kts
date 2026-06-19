c24d7d80c6ac56af1c987443a7de57ece97ed06d
Arisemoss <Arisemoss@users.noreply.github.com>
2026-06-19 16:20:19 +0000

    feat: 开发完整成品
    Co-authored-by: traeagent <traeagent@users.noreply.github.com>

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,5 +1,10 @@
 pluginManagement {
     repositories {
+        mavenLocal()
+        maven { url = uri("https://maven.aliyun.com/repository/google") }
+        maven { url = uri("https://maven.aliyun.com/repository/gradle-plugin") }
+        maven { url = uri("https://maven.aliyun.com/repository/public") }
+        maven { url = uri("https://plugins.gradle.org/m2/") }
         google()
         mavenCentral()
         gradlePluginPortal()
@@ -8,6 +13,9 @@ pluginManagement {
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
+        mavenLocal()
+        maven { url = uri("https://maven.aliyun.com/repository/google") }
+        maven { url = uri("https://maven.aliyun.com/repository/public") }
         google()
         mavenCentral()
     }
